987af19c873fd734ec1f0775eecef02aab86ca91
leonnelc <leonelcortes2424@gmail.com>
2024-03-14 15:41:33 -0300

    Added routes for the views chat and cart

--- clase-8/src/routes/views.router.js
+++ clase-8/src/routes/views.router.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const ProductManager = require("../controllers/ProductManager");
+const getMessages = require("../controllers/ChatManager").getMessages;
 const pm = new ProductManager();
 
 
-router.get("/realtimeproducts", (req, res) => {
+router.get("/realtimeproducts", async (req, res) => {
     res.render("realTimeProducts", {title:"Real time products"});
 })
-router.get("/", (req, res) => {
-    res.render("home", {title:"Home", products:pm.getProducts()});
+router.get("/", async (req, res) => {
+    res.render("home", {title:"Home", products:await pm.getProducts()});
+})
+router.get("/chat", async (req, res) => {
+    res.render("chat", {title:"Chat", messages:await getMessages()});
+})
+router.get("/cart", async (req, res) => {
+    res.render("cart", {tile:"Cart"});
 })
 
 module.exports = router;
