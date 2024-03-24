27f77dce7e02eee6c3e0ad9837b0bf56acf40f70
Leonel Cortes <leonelcortes2424@gmail.com>
2024-03-24 17:27:39 -0300

    Add sort & category selectors

--- clase-9-segunda-entrega/src/public/js/home.js
+++ clase-9-segunda-entrega/src/public/js/home.js
@@ -1,14 +1,26 @@
-function queryRedirect(query, value){
-    // Sets a query and redirects, keeping the other queries. Deletes the query if value is empty.
+function queryRedirect(queries, values){
+    // Sets queries and redirects, keeping the other queries. Deletes the query if value is empty.
     let params = new URLSearchParams(window.location.search);
-    params.set(query, value);
-    if (value == ""){
-        params.delete(query);
+    for (i in queries){
+        params.set(queries[i], values[i]);
+        if (values[i] == ""){
+            params.delete(queries[i]);
+        }
     }
     window.location.search = params.toString();
 }
 const sortSelector = document.getElementById("sort-selector");
-sortSelector.value = new URLSearchParams(window.location.search).get("sort") ?? "";
+const categorySelector = document.getElementById("category-selector");
+let search = new URLSearchParams(window.location.search);
+sortSelector.value = search.get("sort") ?? "";
+categorySelector.value = JSON.parse(search.get("query"))?.category ?? "";
 sortSelector.onchange = (event) => {
-    queryRedirect("sort", event.target.value);
+    queryRedirect(["sort"], [event.target.value]);
+}
+categorySelector.onchange = (event) => {
+    queryRedirect(["query"], [event.target.value != "" ?`{"category":"${event.target.value}"}`: ""]);
+}
+
+function addToCart(id){
+    alert("Not implemented yet");
 }
