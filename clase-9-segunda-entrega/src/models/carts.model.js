967d122b89322e8151b5fc76ad768d943c4ec47f
Leonel Cortes <leonelcortes2424@gmail.com>
2024-03-24 11:23:41 -0300

    Add function to remove null references to products
    Carts model now has a post function that automatically removes null products after populating

--- clase-9-segunda-entrega/src/models/carts.model.js
+++ clase-9-segunda-entrega/src/models/carts.model.js
@@ -11,12 +11,19 @@ const cartSchema = new Schema({
   items: [cartItemSchema],
   total: { type: Number, default: 0 }
 });
-
-cartSchema.pre("find", function(next){
-  console.log("pre");
+cartSchema.pre("findOne", function(next){
   this.populate("items.product");
   next();
 });
+cartSchema.post('findOne', async function(doc) {
+  // removes null products (products with wrong references or that were deleted after being added to the cart)
+  if (doc) {
+    const updatedItems = doc.items.filter(item => item.product !== null);
+    doc.items = updatedItems;
+    await doc.save();
+  }
+});
+
 
 const Cart = mongoose.model('Cart', cartSchema);
 
