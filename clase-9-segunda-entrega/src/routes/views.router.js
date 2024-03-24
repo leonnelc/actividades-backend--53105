76d0546459c952e3fa6f09ae8af80e457a2aa0e6
Leonel Cortes <leonelcortes2424@gmail.com>
2024-03-24 17:30:52 -0300

    Implement cart views

--- clase-9-segunda-entrega/src/routes/views.router.js
+++ clase-9-segunda-entrega/src/routes/views.router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const ProductManager = require("../controllers/ProductManager");
+const CartManager = require("../controllers/CartManager");
 const getMessages = require("../controllers/ChatManager").getMessages;
 const pm = new ProductManager();
+const cm = new CartManager(pm);
 
 function buildQueryString(queryObj, baseString = "", replace={}){
     result = baseString;
@@ -23,11 +25,14 @@ router.get("/realtimeproducts", async (req, res) => {
     try {
         res.render("realTimeProducts", {title:"Real time products"});
     } catch (error) {
-        res.status(500).send({status:"error", message:"Internal server error"})
+        res.status(500).json({status:"error", message:"Internal server error"});
         
     }
 })
 router.get("/", async (req, res) => {
+    res.render("message", {message:"This page is empty, use the navbar to go somewhere else."});
+})
+router.get("/products", async (req, res) => {
     try {
         let limit, page, sort, query;
         req.query.limit != undefined ? limit = Number(req.query.limit): limit = null;
@@ -39,13 +44,13 @@ router.get("/", async (req, res) => {
             try{
                 query = JSON.parse(req.query.query);
             } catch (error){
-                res.status(500).send({status:"error", message:`Error parsing query, make sure it's in JSON format`});
+                res.status(500).json({status:"error", message:`Error parsing query, make sure it's in JSON format`});
                 return;
             }
         }
         let result = await pm.getProductsPaged({limit, page, sort, query});
         if (result.error){
-            res.status(500).send({status:"error", message:result.message});
+            res.status(500).json({status:"error", message:result.message});
             return;
         }
         result.docs = result.docs.map(doc => JSON.parse(JSON.stringify(doc)));
@@ -58,25 +63,37 @@ router.get("/", async (req, res) => {
             nextLink = buildQueryString(req.query, '', {page:result.page+1});
             lastLink = buildQueryString(req.query, '', {page:result.totalPages});
         }
+        const categories = await pm.getUniqueProperty("category");
 
-        res.render("home", {title:"Home", products:result.docs, page, totalPages, prevPage, nextPage, hasPrevPage, hasNextPage, prevLink, nextLink, firstLink, lastLink});
+        res.render("home", {title:"Home", products:result.docs, page, totalPages, prevPage, nextPage, hasPrevPage, hasNextPage, prevLink, nextLink, firstLink, lastLink, categories});
     } catch (error) {
-        res.status(500).send({status:"error", message:"Internal server error"})
+        res.status(500).json({status:"error", message:"Internal server error"});
     }
 })
 router.get("/chat", async (req, res) => {
     try {
         res.render("chat", {title:"Chat", messages:await getMessages()});
     } catch (error) {
-        res.status(500).send({status:"error", message:"Internal server error"})
+        res.status(500).json({status:"error", message:"Internal server error"});
         
     }
 })
-router.get("/cart", async (req, res) => {
+router.get("/carts", async (req, res) => {
+    res.render("message", {message:"You didn't specify the cart id in the url. There's nothing here.", p:"Try /carts/65fe45c44fa17fb49a863d0a"});
+})
+router.get("/carts/:cid", async (req, res) => {
     try {
-        res.render("cart", {tile:"Cart"});
+        const result = await cm.getCartById(req.params.cid);
+        if (result.errmsg !== 0){
+            res.render("cart", {title:"Cart", cartFound:false, cid:req.params.cid});
+            return;
+        }
+        const cart = result.cart;
+        const products = cart.items.map(doc => JSON.parse(JSON.stringify(doc)));
+        const hasProducts = cart.items.length > 0;
+        res.render("cart", {tile:"Cart", cartFound:true, cid:req.params.cid, hasProducts, products});
     } catch (error) {
-        res.status(500).send({status:"error", message:"Internal server error"})
+        res.status(500).json({status:"error", message:"Internal server error"});
         
     }
 })
