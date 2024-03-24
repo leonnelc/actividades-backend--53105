1b3f586a4e221e547adcd4f004f18ba38a415e36
Leonel Cortes <leonelcortes2424@gmail.com>
2024-03-24 17:22:25 -0300

    Implement updateQuantityMany & bugfixes

--- clase-9-segunda-entrega/src/controllers/CartManager.js
+++ clase-9-segunda-entrega/src/controllers/CartManager.js
@@ -24,7 +24,11 @@ class CartManager {
     }
     async getCartById(id){
         try {
-            return {errmsg:0, cart:await cartsModel.findById(id).lean()};
+            const result = await cartsModel.findById(id);
+            if (result == null){
+                return {errmsg:`Cart id ${id} not found`, cart:null};
+            }
+            return {errmsg:0, cart:result};
         } catch (error) {
             if (error.name == "CastError"){
                 return {errmsg:"Invalid product id format specified"};
@@ -43,8 +47,8 @@ class CartManager {
         let cart;
         let product;
         try {
-            cart = await cartsModel.findById(cartId);
-            console.log(`cart: ${cart}`);
+            const result = await cartsModel.find({_id:cartId});
+            cart = result[0];
         } catch (error) {
             if (error.name == "CastError"){
                 return {errmsg:"Invalid cart id format specified"};
@@ -52,7 +56,6 @@ class CartManager {
             return {errmsg:`Error finding cart id ${cartId}: ${error.message}`}
         }
         product = await this.ProductManager.getProductById(prodId);
-        //product = await productsModel.findById(prodId);
         if (cart == null){
             return {errmsg:`Cart id ${cartId} doesn't exist`};
         }
@@ -85,8 +88,8 @@ class CartManager {
         }
         let cart;
         try {
-            cart = await cartsModel.findById(cartId);
-            console.log(`cart: ${cart}`);
+            const result = await cartsModel.find({_id:cartId}); 
+            cart = result[0];
         } catch (error) {
             if (error.name == "CastError"){
                 return {errmsg:"Invalid cart id format specified"};
@@ -94,7 +97,7 @@ class CartManager {
             return {errmsg:`Error finding cart id ${cartId}: ${error.message}`}
         }
         if (cart == null){
-            return {errmsg:`Cart id ${cartId} doesn't exist`};
+            return {errmsg:`Cart id ${cartId} not found`};
         }
         const productIndex = cart.items.findIndex( (item) => {
             return item.product == prodId;
@@ -117,7 +120,8 @@ class CartManager {
     }
     async getCartProducts(cid){
         try {
-            const cart = await cartsModel.findById(cid);
+            const result = await cartsModel.find({_id:cid});
+            const cart = result[0];
             if (cart == null){
                 throw new Error(`Cart with id ${cid} not found`);
             }
@@ -129,6 +133,59 @@ class CartManager {
             return {errmsg:error.message, products:null};
         }
 
+    }
+    async updateQuantity(cid, pid, quantity){
+        try {
+            const result = await cartsModel.find({_id:cid});
+            const cart = result[0];
+            if (cart == null){
+                return {error:true, message:`Cart id ${cid} not found`};
+            }
+            const item = await cart.items.find((doc) => {
+                return doc.product == pid;
+            });
+            if (item == undefined){
+              return {error:true, message:`Couldn't find item id ${pid} in cart ${cid}`};  
+            }
+            item.quantity = quantity;
+            cart.save();
+            return {error:false};
+        } catch (error) {
+            console.log(error)
+            return {error:true, message:error.message};
+        }
+    }
+    async updateQuantityMany(cid, items){
+        try {
+            const result = await cartsModel.find({_id:cid});
+            const cart = result[0];
+            const indexes = new Map(); // [id, index]
+            if (cart == null){
+                return {error:true, message:`Cart id ${cid} not found`};
+            }
+            for (let i in cart.items){
+                let id = cart.items[i].product._id.toString();
+                indexes.set(id, i);
+            }
+            for (let item of items){
+                if (item.product == null || item.quantity == null){
+                    return {error:true, message:`Objects must contain the properties product and quantity`};
+                }
+                if (!indexes.has(item.product)){
+
+                    if (await this.ProductManager.getProductById(item.product) == null){
+                        return {error:true, message:`Product id ${item.product} doesn't exist`};
+                    }
+                    indexes.set(item.product, cart.items.push({product:item.product, quantity:item.quantity}));
+                    continue;
+                }
+                cart.items[indexes.get(item.product)].quantity = item.quantity;
+            }
+            await cart.save();
+            return {error:false};
+        } catch (error) {
+            return {error:true, message:error.message};
+        }
     }
 }
 module.exports = CartManager;
