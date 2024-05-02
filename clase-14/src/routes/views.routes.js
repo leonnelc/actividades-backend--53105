b79f776b4b383b79b66f1daa999b3f1783cd056f
Leonel Cortes <leonelcortes2424@gmail.com>
2024-05-02 01:06:08 -0300

    Implement routers for cart, product, auth & views

---
+++ clase-14/src/routes/views.routes.js
@@ -0,0 +1,25 @@
+const Router = require("express").Router();
+const ViewsController = require("../controllers/ViewsController");
+const {
+  viewsAdminAuth: adminAuth,
+  viewsAuth: auth,
+} = require("../controllers/AuthController");
+
+Router.get("/", auth, ViewsController.products);
+Router.get("/products", auth, ViewsController.products);
+Router.get(
+  "/realtimeproducts",
+  auth,
+  adminAuth,
+  ViewsController.realTimeProducts
+);
+Router.get("/carts/:cid", auth, ViewsController.carts);
+Router.get("/carts", auth, ViewsController.carts);
+Router.get("/cart", auth, ViewsController.carts);
+Router.get("/login", ViewsController.login);
+Router.get("/register", ViewsController.register);
+Router.get("/profile", auth, ViewsController.profile);
+Router.get("/logout", ViewsController.logout);
+Router.get("/chat", auth, ViewsController.chat);
+
+module.exports = Router;
