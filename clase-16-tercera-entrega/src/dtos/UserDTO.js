7ec6edde36dd6f528dfbf02e789aee05d0718e69
Leonel Cortes <leonelcortes2424@gmail.com>
2024-05-18 16:31:10 -0300

    fix: add space between first and last name in UserDTO

--- clase-16-tercera-entrega/src/dtos/UserDTO.js
+++ clase-16-tercera-entrega/src/dtos/UserDTO.js
@@ -1,7 +1,7 @@
 class UserDTO {
   constructor(user) {
     this.id = user._id;
-    this.full_name = user.first_name + (user.last_name ?? "");
+    this.full_name = `${user.first_name} ${user.last_name ?? ""}`;
     this.first_name = user.first_name;
     this.last_name = user.last_name ?? null;
     this.email = user.email;
