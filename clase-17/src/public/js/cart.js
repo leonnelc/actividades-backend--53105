092efa4a93d58e399eab2b240b231a65faaf2d13
Leonel Cortes <leonelcortes2424@gmail.com>
2024-05-28 00:56:17 -0300

    feat(views): add cart purchase button & ticket modal

--- clase-17/src/public/js/cart.js
+++ clase-17/src/public/js/cart.js
@@ -12,6 +12,91 @@ try {
     console.log(session);
   });
 }
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms)); //for testing only
+async function purchaseCart(cid) {
+  const loadingCircle = document.createElement("div");
+  loadingCircle.classList.add(
+    "spinner-border",
+    "text-primary",
+    "position-absolute",
+    "top-50",
+    "start-50"
+  );
+  loadingCircle.setAttribute("role", "status");
+  const loadingCircleSpan = document.createElement("span");
+  loadingCircleSpan.classList.add("visually-hidden");
+  loadingCircleSpan.textContent = "Loading...";
+  loadingCircle.appendChild(loadingCircleSpan);
+  const loadingBackdrop = document.createElement("div");
+  loadingBackdrop.classList.add("modal-backdrop", "fade", "show");
+  document.body.appendChild(loadingBackdrop);
+  document.body.appendChild(loadingCircle);
+  let purchase;
+  try {
+    const response = await fetch(`/api/carts/${cid}/purchase`, {
+      method: "POST",
+    });
+    purchase = await response.json();
+    if (purchase.status == "error") {
+      throw new Error(purchase.message);
+    }
+  } catch (error) {
+    return appendAlert(error.message, "danger", 3000);
+  } finally {
+    document.body.removeChild(loadingBackdrop);
+    document.body.removeChild(loadingCircle);
+  }
+
+  const purchaseModal = new bootstrap.Modal(
+    document.getElementById("purchaseModal")
+  );
+  const ticketCode = document.getElementById("ticketCode");
+  const purchaseDate = document.getElementById("purchaseDate");
+  const purchaseAmount = document.getElementById("purchaseAmount");
+  const purchaser = document.getElementById("purchaser");
+  const notPurchasedItemsList = document.getElementById(
+    "notPurchasedItemsList"
+  );
+  const purchasedItemsList = document.getElementById("purchasedItemsList");
+
+  ticketCode.textContent = purchase.ticket.code;
+  purchaseDate.textContent = new Date(
+    purchase.ticket.purchase_datetime
+  ).toLocaleString();
+  purchaseAmount.textContent = purchase.ticket.amount;
+  purchaser.textContent = purchase.ticket.purchaser;
+  // Clear the not purchased items list
+  notPurchasedItemsList.innerHTML = "";
+  purchasedItemsList.innerHTML = "";
+
+  // Add not purchased items to the list
+  if (purchase.not_purchased && purchase.not_purchased.length > 0) {
+    purchase.not_purchased.forEach((item) => {
+      const li = document.createElement("li");
+      const product = document.getElementById(item.id);
+      const quantity = item.requiredStock;
+      li.textContent = `${product.getAttribute("data-title")} x${quantity}`;
+      notPurchasedItemsList.appendChild(li);
+    });
+  } else {
+    const li = document.createElement("li");
+    li.textContent = "No items were not purchased.";
+    notPurchasedItemsList.appendChild(li);
+  }
+  // Delete purchased items from the cart page
+  if (purchase.purchased && purchase.purchased.length > 0) {
+    purchase.purchased.forEach((item) => {
+      const li = document.createElement("li");
+      const product = document.getElementById(item.id);
+      const quantity = item.quantity;
+      li.textContent = `${product.getAttribute("data-title")} x${quantity}`;
+      purchasedItemsList.appendChild(li);
+      product.remove();
+    });
+  }
+
+  purchaseModal.show();
+}
 async function getSession() {
   const response = await fetch("/api/sessions/current", { method: "GET" });
   session = await response.json();
