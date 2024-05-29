d2d06af4ec49a4bfbf1915e0eddd97fbc7d039a7
Leonel Cortes <leonelcortes2424@gmail.com>
2024-05-29 00:50:54 -0300

    Moved clase-5-primera-entrega to root and deleted every other directory

--- clase-17/src/controllers/MockingController.js
+++
@@ -1,8 +0,0 @@
-const MockingService = require("../services/MockingService");
-async function mockingProducts(req, res) {
-  res.json({
-    status: "success",
-    products: MockingService.createMultipleProducts(100),
-  });
-}
-module.exports = { mockingProducts };
--- clase-17/src/services/errors/CustomError.js
+++
@@ -1,9 +0,0 @@
-class CustomError extends Error {
-  constructor(message, info = { name: "Error", data: null }) {
-    super(message);
-    this.name = info.name;
-    this.data = info.data;
-  }
-}
-
-module.exports = CustomError;
