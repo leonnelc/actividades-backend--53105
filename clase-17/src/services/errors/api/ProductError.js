8abfa361fc5dbcde499409b193632bcdc6bc8940
Leonel Cortes <leonelcortes2424@gmail.com>
2024-05-28 22:46:02 -0300

    Remove all clases and move class-1 to root

--- clase-17/src/services/errors/api/ProductError.js
+++
@@ -1,8 +0,0 @@
-const APIError = require("../APIError");
-class ProductError extends APIError {
-  constructor(message, info = { name: "ProductError", data: undefined }) {
-    super(message, { name: info.name, data: info.data });
-  }
-}
-
-module.exports = ProductError;
