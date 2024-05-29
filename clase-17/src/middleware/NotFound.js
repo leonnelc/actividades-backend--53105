623ffb0e069b64ef7b804ce8435f4707fe33bb67
Leonel Cortes <leonelcortes2424@gmail.com>
2024-05-29 01:04:10 -0300

    Moved clase-13 to root and deleted every other directory

--- clase-17/src/middleware/NotFound.js
+++
@@ -1,8 +0,0 @@
-function NotFound(req, res) {
-  res.status(404).render("message", {
-    message: "404 Page not found",
-    error: true,
-    title: "404 Not found",
-  });
-}
-module.exports = NotFound;
