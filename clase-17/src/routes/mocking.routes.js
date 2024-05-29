557d1650bff6793534994df5251d77469fd34c04
Leonel Cortes <leonelcortes2424@gmail.com>
2024-05-29 01:00:31 -0300

    Moved clase-11 to root and deleted every other directory

--- clase-17/src/routes/mocking.routes.js
+++
@@ -1,6 +0,0 @@
-const Router = require("express").Router();
-const MockingController = require("../controllers/MockingController");
-
-Router.get("/mockingproducts", MockingController.mockingProducts);
-
-module.exports = Router;
--- clase-17/src/services/errors/api/CartError.js
+++
@@ -1,8 +0,0 @@
-const APIError = require("../APIError");
-class CartError extends APIError {
-  constructor(message, info = { name: "CartError", data: null }) {
-    super(message, info);
-  }
-}
-
-module.exports = CartError;
