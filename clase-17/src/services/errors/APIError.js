99d2650bd86e2457767b27160c45e356e815633b
Leonel Cortes <leonelcortes2424@gmail.com>
2024-05-29 00:45:06 -0300

    Moved clase-3 to root and deleted every other directory

--- clase-17/src/services/errors/APIError.js
+++
@@ -1,8 +0,0 @@
-const CustomError = require("./CustomError");
-class APIError extends CustomError {
-  constructor(message, info = { name: "APIError", data: null }) {
-    super(message, info);
-  }
-}
-
-module.exports = APIError;
