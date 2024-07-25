2b8fc596265a99485e829a5c7baae6de01b07a26
leonnelc <leonelcortes2424@gmail.com>
2024-07-25 15:01:38 -0300

    feat(api): add rate limiter to login & register

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,29 @@
 const Router = require("express").Router();
 const AuthController = require("../controllers/AuthController");
+const { rateLimit } = require("express-rate-limit");
 const checkRoles = AuthController.checkRoles;
 const checkNotLoggedIn = checkRoles(["notloggedin"]);
 
-Router.post("/login", checkNotLoggedIn, AuthController.loginLocal);
-Router.post("/register", checkNotLoggedIn, AuthController.registerLocal);
+Router.post(
+  "/login",
+  rateLimit({
+    limit: 5,
+    windowMs: 60 * 1000,
+    message: { message: "Too many login attempts, try again in 1 minute." },
+  }),
+  checkNotLoggedIn,
+  AuthController.loginLocal,
+);
+Router.post(
+  "/register",
+  rateLimit({
+    limit: 5,
+    windowMs: 60 * 1000,
+    message: { message: "Too many register attempts, try again in 1 minute." },
+  }),
+  checkNotLoggedIn,
+  AuthController.registerLocal,
+);
 Router.get("/google", AuthController.googleLogin);
 Router.get("/google/callback", AuthController.googleCallback);
 Router.get("/github", AuthController.githubLogin);
